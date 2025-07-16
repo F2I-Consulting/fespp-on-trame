1fdfb28a043c2108af328c503bba9412b5a845b7
cedricRobert <cedric.robert@f2i-consulting.com>
2025-07-16 16:34:16 +0200

    use FESAPI_ROOT for build FESPP

--- fespp/build_scripts/build-fespp.sh
+++ fespp/build_scripts/build-fespp.sh
@@ -9,9 +9,7 @@ cd ${build_root_dir}/build-fespp/
 cmake \
     -DPARAVIEW_PLUGIN_ENABLE_Fespp=ON \
     -DCMAKE_INSTALL_PREFIX=/work/ttl/install-fespp/ \
-    -DFESAPI_INCLUDE_DIR=/work/ttl/install-fesapi/include \
-    -DFESAPI_LIBRARY_DEBUG=/work/ttl/install-fesapi/lib/libFesapiCpp.so \
-    -DFESAPI_LIBRARY_RELEASE=/work/ttl/install-fesapi/lib/libFesapiCpp.so \
+    -DFESAPI_ROOT=/work/ttl/install-fesapi \
     -DParaView_DIR=/work/pvsb-build/superbuild/paraview/build \
     -Dnlohmann_json_DIR=/work/pvsb-build/install/lib/cmake/nlohmann_json \
     -DSQLite3_INCLUDE_DIR=/work/pvsb-build/install/include \
