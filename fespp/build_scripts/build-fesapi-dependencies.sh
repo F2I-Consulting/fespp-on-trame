50ccb1f0f30eabce9b33c0fa3765ad2ad2fd8378
cedricRobert <cedric.robert@f2i-consulting.com>
2025-10-01 09:40:53 +0200

    Optimize ParaView/Fesapi/Fespp builds to eliminate unnecessary full rebuilds

--- fespp/build_scripts/build-fesapi-dependencies.sh
+++ fespp/build_scripts/build-fesapi-dependencies.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-
 build_root_dir=${FESPP_BUILD_ROOT_DIR:-"/work/ttl"}
 deps_dir=${build_root_dir}/dependencies
 mkdir -p $deps_dir
 cd $deps_dir
-
 git clone https://github.com/F2I-Consulting/Minizip.git
-mkdir build-minizip
-mkdir install-minizip
-cd build-minizip/
-cmake -DCMAKE_INSTALL_PREFIX=/work/ttl/dependencies/install-minizip /work/ttl/dependencies/Minizip
+mkdir -p $deps_dir/build-minizip
+mkdir -p $deps_dir/install-minizip
+cd $deps_dir/build-minizip
+cmake -DCMAKE_INSTALL_PREFIX=$deps_dir/install-minizip $deps_dir/Minizip
 cmake --build . --parallel
 cmake --install .
